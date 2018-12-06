b254e1b232052d51e5246802b67780fe67b507c8
yacinedexter <yacine.rsd2@gmail.com>
2018-12-06 18:42:27 -0500

    Update ner_model.py

--- model/ner_model.py
+++ model/ner_model.py
@@ -315,7 +315,7 @@ class NERModel(BaseModel):
         correct_preds, total_correct, total_preds = 0., 0., 0.
         
         print("Writing test Résults...")
-        with open("data/chars.txt", "w") as f:
+        with open("data/results.txt, "w") as f:
             for words, labels in minibatches(test, self.config.batch_size):
                 labels_pred, sequence_lengths = self.predict_batch(words)
 
