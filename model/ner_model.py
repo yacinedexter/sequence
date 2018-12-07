d32372ce137309aaa1f8c036a8aa061dbf101eba
yacinedexter <yacine.rsd2@gmail.com>
2018-12-06 19:16:55 -0500

    Update ner_model.py

--- model/ner_model.py
+++ model/ner_model.py
@@ -325,7 +325,7 @@ class NERModel(BaseModel):
                     lab_pred = lab_pred[:length]
                     accs    += [a==b for (a, b) in zip(lab, lab_pred)]
                     for (a, b) in zip(lab, lab_pred):
-                        f.write('(lable : '+a+' -> labelPred : '+b+')\n')
+                        f.write('(lable : '+str(a)+' -> labelPred : '+str(b)+')\n')
                     f.write('\n\n')
                     lab_chunks      = set(get_chunks(lab, self.config.vocab_tags))
                     lab_pred_chunks = set(get_chunks(lab_pred,
