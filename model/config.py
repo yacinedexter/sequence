72055c0eed8d312bb88dc0fa0539f4d0e514c959
yacinedexter <yacine.rsd2@gmail.com>
2018-12-17 11:20:03 -0500

    Update config.py

--- model/config.py
+++ model/config.py
@@ -89,7 +89,7 @@ class Config():
     train_embeddings = False
     nepochs          = 50
     dropout          = 0.5
-    batch_size       = 3
+    batch_size       = 5
     lr_method        = "adam"
     lr               = 0.01
     lr_decay         = 0.9
