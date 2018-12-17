67ec15881f8eb10c82a2c17984576d4858485ca8
yacinedexter <yacine.rsd2@gmail.com>
2018-12-16 22:42:30 -0500

    Update config.py

--- model/config.py
+++ model/config.py
@@ -90,7 +90,7 @@ class Config():
     nepochs          = 50
     dropout          = 0.5
     batch_size       = 5
-    lr_method        = "adam"
+    lr_method        = "sgd"
     lr               = 0.03
     lr_decay         = 0.9
     clip             = -1 # if negative, no clipping
