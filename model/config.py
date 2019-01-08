cc767512d6fb0c1c877ce439e95289092a217b37
yacinedexter <yacine.rsd2@gmail.com>
2019-01-07 23:23:42 -0500

    Update config.py

--- model/config.py
+++ model/config.py
@@ -98,7 +98,7 @@ class Config():
 
     # model hyperparameters
     hidden_size_char = 100 # lstm on chars
-    hidden_size_lstm = 400 # lstm on word embeddings
+    hidden_size_lstm = 500 # lstm on word embeddings
 
     # NOTE: if both chars and crf, only 1.6x slower on GPU
     use_crf = True # if crf, training is 1.7x slower on CPU
