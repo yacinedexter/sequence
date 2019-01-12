2b02e97005f8bf2eb3bb1ce2bf9630954b098f3d
yacinedexter <yacine.rsd2@gmail.com>
2019-01-12 12:57:47 -0500

    Update config.py

--- model/config.py
+++ model/config.py
@@ -86,7 +86,7 @@ class Config():
     filename_results = "data/results.txt"
 
     # training
-    train_embeddings = True
+    train_embeddings = False
     nepochs          = 50
     dropout          = 0.5
     batch_size       = 2
